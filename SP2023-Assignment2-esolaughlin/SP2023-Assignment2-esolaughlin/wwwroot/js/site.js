b5b32152df23d8a2bf1a33d4e91a1bab59b3b874
esolaughlin <esolaughlin@crimson.ua.edu>
2023-02-15 21:58:09 -0600

    bruno!!

--- SP2023-Assignment2-esolaughlin/SP2023-Assignment2-esolaughlin/wwwroot/js/site.js
+++ SP2023-Assignment2-esolaughlin/SP2023-Assignment2-esolaughlin/wwwroot/js/site.js
@@ -1,7 +1,10 @@
-﻿var len;
+﻿console.log("Here")
+
+var len;
 var results = '';
 
 function apiSearch() {
+    debugger;
     var params = {
         "q": $("#query").val(),
         "count": "50",
@@ -10,9 +13,9 @@ function apiSearch() {
     };
 
     $.ajax({
-        url: 'my-api-url' + $.param(params),
+        url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
         beforeSend: function (xhrObj) {
-            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "my-api-key");
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "44212b496dff4ec481617cea2eb49599");
         },
         type: "GET",
     })
@@ -21,11 +24,19 @@ function apiSearch() {
             for (i = 0; i < len; i++) {
                 results += "<p><a href='" + data.webPages.value[i].url + "'>" + data.webPages.value[i].name + "</a>: " + data.webPages.value[i].snippet + "</p>";
             }
-
+            debugger
             $('#searchResults').html(results);
+            $('#searchResults').css("visibility", "visible")
+            document.getElementById("searchResults").innerHTML = results
             $('#searchResults').dialog();
         })
         .fail(function () {
             alert("error");
         });
 }
+
+$("#searchBtn").click(function () { 
+    apiSearch();
+});
+
+
